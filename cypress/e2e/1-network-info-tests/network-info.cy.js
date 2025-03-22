080e68f343327b30412b11359a73e19a1d5466a6
Justin Looi <justinlooijw@gmail.com>
2025-03-22 22:57:06 +0800

    Add initial project setup with Cypress configuration, fixtures, and favicon

---
+++ cypress/e2e/1-network-info-tests/network-info.cy.js
@@ -0,0 +1,8 @@
+describe('Network Info Tracker', () => {
+  it('should load and show the heading', () => {
+    cy.visit('/'); // Replace with your local dev URL
+    cy.contains('Network Information Tracker');
+  });
+
+  
+});
